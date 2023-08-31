628714487c83b5a7b4ed72a745657cb31a875851
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2023-08-30 22:36:12 -0300

    Criação Da API do projeto utilizando Spring Boot

---
+++ api_gestao_de_lancamentos_financeiros/src/main/java/com/example/ApiGestaoDeLancamentosFinanceirosApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestaoDeLancamentosFinanceirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestaoDeLancamentosFinanceirosApplication.class, args);
+	}
+
+}
