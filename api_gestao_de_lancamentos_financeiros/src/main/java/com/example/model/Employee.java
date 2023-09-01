e08c59ae955833d47b07ab5675110e08195c4037
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2023-08-31 23:19:12 -0300

    criação da classe de funcionário e da migration corresponde para a criação da tabela de funcionário no banco de dados

---
+++ api_gestao_de_lancamentos_financeiros/src/main/java/com/example/model/Employee.java
@@ -0,0 +1,88 @@
+package com.example.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TABLE_EMPLOYEES")
+public class Employee implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "full_name", nullable = false)
+	private String fullName;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String agency;
+	
+	@Column(name = "checking_account", nullable = false, unique = true)
+	private String checkingAccount;
+	
+	
+	public Employee() {
+		
+	}
+
+	public Employee(Long id, String fullName, String email, String agency, String checkingAccount) {
+		this.id = id;
+		this.fullName = fullName;
+		this.email = email;
+		this.agency = agency;
+		this.checkingAccount = checkingAccount;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAgency() {
+		return agency;
+	}
+
+	public void setAgency(String agency) {
+		this.agency = agency;
+	}
+
+	public String getCheckingAccount() {
+		return checkingAccount;
+	}
+
+	public void setCheckingAccount(String checkingAccount) {
+		this.checkingAccount = checkingAccount;
+	}
+	
+}
