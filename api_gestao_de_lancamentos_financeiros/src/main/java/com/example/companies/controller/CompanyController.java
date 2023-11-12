b8ae9645d2340d58e55341b7e8439853733a3eb8
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2023-11-11 23:08:59 -0300

    Criação do controller de companies

--- api_gestao_de_lancamentos_financeiros/src/main/java/com/example/companies/controller/CompanyController.java
+++ api_gestao_de_lancamentos_financeiros/src/main/java/com/example/companies/controller/CompanyController.java
@@ -1,5 +1,50 @@
 package com.example.companies.controller;
 
-public class CompanyController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.companies.DTO.UpdateCompanyDTO;
+import com.example.companies.model.Company;
+import com.example.companies.service.CompanyService;
+
+@RequestMapping("/companies")
+@CrossOrigin
+@RestController
+public class CompanyController {
+	
+	@Autowired
+	private CompanyService companyService;
+	
+	
+	public ResponseEntity<Company> createCompany(@RequestBody Company company){
+		return companyService.createCompany(company);
+	}
+	
+	public ResponseEntity<Company> findCompanyById(@PathVariable Long id){
+		return companyService.findCompanyById(id);
+	}
+	
+	public ResponseEntity<Page<Company>> findAllCompanies(
+		@RequestParam int page, 
+		@RequestParam int limit, 
+		@RequestParam String direction
+	){
+		return companyService.findAllCompanies(page, limit, direction);
+	}
+	
+	public ResponseEntity<Company> updateCompany(@RequestBody UpdateCompanyDTO company){
+		return companyService.updateCompany(company);
+	}
+	
+	public ResponseEntity<Company> deleteCompanybyId(@PathVariable Long id){
+		return companyService.deleteCompanyById(id);
+	}
+	
 }
