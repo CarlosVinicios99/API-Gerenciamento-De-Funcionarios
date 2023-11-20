28d6e233a29d6d2ac3e6ce16396f7f049ae2579a
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2023-11-20 15:13:38 -0300

    Criação de serviço para buscar funcionário por ID

--- api_gestao_de_lancamentos_financeiros/src/main/java/com/example/employees/service/EmployeeService.java
+++ api_gestao_de_lancamentos_financeiros/src/main/java/com/example/employees/service/EmployeeService.java
@@ -36,4 +36,20 @@ public class EmployeeService {
 		}
 	}
 	
+	public ResponseEntity<Employee> findByEmployeeById(Long id){
+		this.logger.log(Level.INFO, "Buscar um funcionário por ID");
+		try {
+			Employee searchedEmployee = employeeRepository.findById(id).get();
+			if(searchedEmployee != null) {
+				return ResponseEntity.ok().body(searchedEmployee);
+			}
+			logger.log(Level.WARNING, "Nenhum funcionário encontrado!");
+			return ResponseEntity.noContent().build();
+		}
+		catch(Exception error) {
+			this.logger.log(Level.SEVERE, "Erro ao buscar funcionário por ID");
+			return ResponseEntity.internalServerError().build();
+		}
+	}
+	
 }
